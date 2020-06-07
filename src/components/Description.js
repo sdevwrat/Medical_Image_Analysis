82253d8e467ab637b700ab722c4e978918974e30
Devwrat Sahu <61136667+sdevwrat@users.noreply.github.com>
2020-06-07 15:54:07 +0530

    Update Description.js

--- src/components/Description.js
+++ src/components/Description.js
@@ -5,7 +5,8 @@ const Description = () => {
             <div className="intro">
               <div className="intro--banner">
                 <h1>Medical Image<br/>Analysis</h1>
-                <p className="z-1">gdvcgdv jcbdhvbsjh vsdhbcdhbsvj sv shvbhjbsv<br/> shsbdchsdn vsnvbhsdbv dsnvkshbchjs nvsydvhj sn vysgydfbnlka<br/> fsuifeh s,vs ecsbebk</p>              </div>
+                <p className="z-1">Medical imaging is the technique and process of creating visual representations of the interior of a body for clinical analysis and medical intervention, as well as visual representation of the function of some organs or tissues . Medical imaging seeks to reveal internal structures hidden by the skin and bones, as well as to diagnose and treat disease.</p>
+              </div>
             </div>
 	); 
 }

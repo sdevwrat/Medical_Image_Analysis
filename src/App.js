42d13e1dfcea118e0a9063f7d6cc4c2b641b7844
Devwrat Sahu <61136667+sdevwrat@users.noreply.github.com>
2020-06-07 15:43:22 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component{
   render(){
   return (
     <div className="App">
-      <a href='#0' className="pointer"><img className="logo" src={logo} alt='logo'/></a>
+      <a href='https://medinform.tk/' className="pointer"><img className="logo" src={logo} alt='logo'/></a>
       <Description />
       <Cards />
       <Footer />

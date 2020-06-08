20500618f3789a12aec092d8a6304f77c831957c
Guramritpal Singh Saggu <36269200+guramritpalsaggu@users.noreply.github.com>
2020-06-09 00:12:57 +0530

    Update Description.js

--- src/components/Description.js
+++ src/components/Description.js
@@ -5,7 +5,7 @@ const Description = () => {
             <div className="intro">
               <div className="intro--banner">
                 <h1>Medical Image<br/>Analysis</h1>
-                <p className="z-1">Medical imaging is the technique and process of creating visual representations of the interior of a body for clinical analysis and medical intervention, as well as visual representation of the function of some organs or tissues . Medical imaging seeks to reveal internal structures hidden by the skin and bones, as well as to diagnose and treat disease.</p>
+                <p className="z-1">MedInform is an AI Powered Multipurpose platform which provides extensive analysis and ease of access to medical diagnosis by leveraging the power of deep learning.</p>
               </div>
             </div>
 	); 

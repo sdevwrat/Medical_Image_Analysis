76a8a57d5704b3f11be9a2b1cd34716d01ff181f
Devwrat Sahu <61136667+sdevwrat@users.noreply.github.com>
2020-06-08 22:47:31 +0530

    Update Cards.js

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -9,52 +9,22 @@ import Malaria from '../Images/Malaria.jpg';
 
 const Cards = () => {
 	return(
-    <section class="wrapper style1">
-        <div class="inner">
-        <h1 className="light-green b f1">Services</h1>
-          <article class="feature left">
-            <span class="image"><img src={covid} alt="" /></span>
-            <div class="content">
-              <h1>Covid-19</h1>
-              <p>It is the most common human malignancy, is primarily diagnosed visually, beginning with an initial clinical screening and followed potentially by dermoscopic analysis and histopathological examination. Detecting it in early stages can help prevent it from being lethal.</p>
-              <p className="read-more">
-               <a href="https://covid.medinform.tk/">Go to Page</a>
-              </p>
-            </div>
-          </article>
-          <article class="feature right">
-            <span class="image"><img src={tuber} alt="" /></span>
-            <div class="content">
+    <section className="wrapper style1">
+        <div className="inner">
+        <h1 className="light-green f1">Services</h1>
+          <article className="feature left">
+            <span className="image"><img src={tuber} alt="Tuberculosis" /></span>
+            <div className="content">
               <h1>Tuberculosis</h1>
-              <p>It affects many people in developing countries and treating it requires an accurate diagnosis,An algorithm that could perform this task quickly and cheaply could drastically improve the ability to diagnose and ultimately treat the disease.</p>
+              <p>A potentially serious infectious bacterial disease that mainly affects the lungs.It affects many people in developing countries and treating it requires an accurate diagnosis,An algorithm that could perform this task quickly and cheaply could drastically improve the ability to diagnose and ultimately treat the disease.</p>
               <p className="read-more">
                <a href="https://tbc.medinform.tk/">Go to Page</a>
               </p>
             </div>
           </article>
-          <article class="feature left">
-            <span class="image"><img src={Pneumonia} alt="" /></span>
-            <div class="content">
-              <h1>Pneumonia</h1>
-              <p>It is an inflammatory condition of the lung  and is usually caused by infection with viruses or bacteria and less commonly by other microorganisms.Diagnosis is often based on symptoms and physical examination. Chest X-rays help confirm the diagnosis.</p>
-              <p className="read-more">
-               <a href="https://pneumonia.medinform.tk/">Go to Page</a>
-              </p>
-            </div>
-          </article>
-          <article class="feature right">
-            <span class="image"><img src={retina} alt="" /></span>
-            <div class="content">
-              <h1>Diabetic retinopathy </h1>
-              <p>It is the most common and insidious microvascular complication of diabetes, and can progress asymptomatically until a sudden loss of vision occurs.Deep convolutional neural networks (DCNNs),8 can be used for an end-to-end assessment of raw medical images to produce a target outcome prediction.</p>
-              <p className="read-more">
-               <a href="https://diabetes.medinform.tk/">Go to Page</a>
-              </p>
-            </div>
-          </article>
-          <article class="feature left">
-            <span class="image"><img src={skin} alt="" /></span>
-            <div class="content">
+          <article className="feature right">
+            <span className="image"><img src={skin} alt="skin cancer" /></span>
+            <div className="content">
               <h1>Skin Cancer</h1>
               <p>It is the most common human malignancy, is primarily diagnosed visually, beginning with an initial clinical screening and followed potentially by dermoscopic analysis and histopathological examination. Detecting it in early stages can help prevent it from being lethal.</p>
               <p className="read-more">
@@ -62,14 +32,44 @@ const Cards = () => {
               </p>
             </div>
           </article>
-          <article class="feature right">
-            <span class="image"><img src={Malaria} alt="" /></span>
-            <div class="content">
+          <article className="feature left">
+            <span className="image"><img src={Malaria} alt="malaria" /></span>
+            <div className="content">
               <h1>Malaria</h1>
-              <p>It is a deadly, infectious mosquito-borne disease caused by Plasmodium parasites transmitted by the bites of infected female Anopheles mosquitoes.Malaria detection is an intensive manual process which can perhaps be automated using Deep Learning to achieve swift and accurate diagnosis. 
-</p>
+              <p>It is a deadly, infectious mosquito-borne disease caused by Plasmodium parasites transmitted by the bites of infected female Anopheles mosquitoes.Malaria detection is an intensive manual process which can perhaps be automated using Deep Learning to achieve swift and accurate diagnosis. </p>
               <p className="read-more">
                <a href="https://malaria.medinform.tk/">Go to Page</a>
+              </p>
+            </div>
+          </article>
+          <article className="feature right">
+            <span className="image"><img src={covid} alt="covid" /></span>
+            <div className="content">
+              <h1>Covid-19</h1>
+              <p>Coronavirus is a family of viruses that can cause illness, which can vary from common cold and cough to sometimes more severe disease.SARS-CoV-2 is the new virus of the coronavirus family, which first discovered in 2019.This app predicts the probabability of person suffering from Covid-19
+by analysing chest x-rays.</p>
+              <p className="read-more">
+               <a href="https://covid.medinform.tk/">Go to Page</a>
+              </p>
+            </div>
+          </article>
+          <article className="feature left">
+            <span className="image"><img src={Pneumonia} alt="pneumonia" /></span>
+            <div className="content">
+              <h1>Pneumonia</h1>
+              <p>It is an inflammatory condition of the lung  and is usually caused by infection with viruses or bacteria and less commonly by other microorganisms.Diagnosis is often based on symptoms and physical examination. Chest X-rays help confirm the diagnosis.</p>
+              <p className="read-more">
+               <a href="https://pneumonia.medinform.tk/">Go to Page</a>
+              </p>
+            </div>
+          </article>
+          <article className="feature right">
+            <span className="image"><img src={retina} alt="Diabetic retinopathy" /></span>
+            <div className="content">
+              <h1>Diabetic Retinopathy </h1>
+              <p>It is the most common and insidious microvascular complication of diabetes, and can progress asymptomatically until a sudden loss of vision occurs.Deep CNNs,8 can be used for an end-to-end assessment of raw medical images to produce a target outcome prediction.</p>
+              <p className="read-more">
+               <a href="https://diabetes.medinform.tk/">Go to Page</a>
               </p>
             </div>
           </article>
